5aec0f1de45b6a7534b82793f8e6743c41e195b6
Ananda Aulia <72482619+Ananda28@users.noreply.github.com>
2022-01-09 00:25:01 +0700

    dashboard ekstra

---
+++ app/Http/Controllers/Admin/ContentManagement/Dashboard/EkstraController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\ContentManagement\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ContentManagement\Dashboard\Ekstra;
+
+class EkstraController extends Controller
+{
+    //
+    public function index()
+    {
+        $data = Ekstra::where('active',1)->get();
+        return view('admin.dashboard.ekstra.index',compact('data'));
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController;
 use  App\Http\Controllers\Admin\ContentManagement\Dashboard\SambutanController;
+use  App\Http\Controllers\Admin\ContentManagement\Dashboard\EkstraController;
 use App\Http\Controllers\Admin\ContentManagement\Fasilitas\RuangLabController;
 use App\Http\Controllers\Admin\ContentManagement\Berita\ContentBeritaController;
 
@@ -12,9 +13,11 @@ Route::group(['middleware' => ["Admin"], 'as' => 'adm.'], function() {
     Route::get('/admin',[DashboardController::class,'index'])->name('dashboardadmin');
     // Conten Management Dashboard Sambutan
     Route::get('/admin/dashboard/sambutan',[SambutanController::class,'index'])->name('sambutan');
+    // Conten Management Dashboard Ekstrakulikuler
+    Route::get('/admin/dashboard/ekstra',[EkstraController::class,'index'])->name('ekstra');
     // Fasilitas Ruang Lab
     Route::get('/admin/fasilitas/ruanglab',[RuangLabController::class,'index'])->name('ruanglab');
     // Content Berita
     Route::get('/admin/berita/contentberita',[ContentBeritaController::class,'index'])->name('contentberita');
-    
+
 });
